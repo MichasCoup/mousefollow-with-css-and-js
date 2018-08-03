096e2cc4d35c4abf06d8d1cae475c894b299d9b1
Michael Flach <vuti@gmx.de>
2018-08-03 20:05:34 +0200

    update JS

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,46 +1,34 @@
-var BlurAnimation = function() {
+var Blur = function() {
 
-  var mouseX, mouseY, textX, textY;
+ var mouseX, mouseY;
 
-  var _init = function() {
-    mouseX    = 0;
-    mouseY    = 0;
-    slider    = document.getElementsByTagName('body');
-    textX     = document.getElementsByClassName('blur')[0];
-    textY     = document.getElementsByClassName('blur')[0];
-
-    _addEventHandlers();
-  }
-
-  var _addEventHandlers = function() {
-    window.addEventListener('mousemove', _getMousePos, false);
-  }
-
-  var _accelerometerUpdate = function(e) {
-    xPosition = Math.round(xPosition * 1000) / 1000;
-    yPosition = Math.round(yPosition * 1000) / 1000;
-  }
-
-  var _getMousePos = function(e) {
-    e = e || window.event;
-
-    mouseX = e.pageX;
-    mouseY = e.pageY;
-
-    var xPos = (mouseX / window.innerWidth) - 0.5;
-    var yPos = (mouseY / window.innerHeight) - 0.5;
-    var rotationYValue = 5 * xPos;
-    var rotationXValue = 5 * yPos;
-
-    _animate(rotationYValue,rotationXValue);
-    textX.innerHTML = mouseX;
-    textY.innerHTML = mouseY;
-  }
+ // Nimm die letze Position auf
+ var _init = function() {
+   mouseX = 0;
+   mouseY = 0;
+   mouseMove();
+ }
 
 
-  return {
-    init: _init
-  }
+// wenn Maus sich bewegt ...
+ var mouseMove = function() {
+  window.addEventListener('mousemove', getMousePos, false);
+ }
+
+// ... hole die Position der Maus
+ var getMousePos = function(e) {
+  e = e || window.event;
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+
+// gib die Mausposition aus
+  console.log(mouseX + " " + mouseY);
+ }
+
+// Gib die Position zurück
+ return {
+  init: _init
+ }
 }();
 
-BlurAnimation.init();
+Blur.init();
